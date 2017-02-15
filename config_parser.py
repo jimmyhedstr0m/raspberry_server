2c4237686853a1a87d6b186bff624c70b882e72f
Jimmy Hedström <jimmy.hedstrom@hotmail.com>
2017-02-15 18:47:08 +0100

    Modified ir_remote send method

--- config_parser.py
+++ config_parser.py
@@ -20,6 +20,16 @@ class ConfigParser():
     def get_switch_id(self, switch_ix):
         return self.config_data["switch_groups"][switch_ix]["remote_code"]
 
+    def get_remote(self, remote_id):
+        try:
+            remote_id = int(remote_id)
+            if remote_id > 0 and remote_id < len(self.config_data["ir_remotes"]):
+                return self.config_data["ir_remotes"][remote_id]
+            else:
+                return False
+        except ValueError:
+            abort(400)
+
     def get_remote_commands(self, remote_ix):
         return self.config_data["ir_remotes"][remote_ix]["keys"]
 
--- ir_remote.py
+++ ir_remote.py
@@ -1,21 +1,43 @@
 import subprocess
 from config_parser import ConfigParser
-from remote_interface import Remote
 
 
-class IrRemote(Remote):
+class IrRemote():
 
-    def __init__(self, remote_id):
-        Remote.__init__(self, remote_id)
-        self.conf_file = "/etc/lirc/toshiba_tv_remote.conf"
+    def __init__(self):
+        self.config_parser = ConfigParser()
+        self._max_steps = 80
 
-    def execute_command(self, command_id):
-        if self.validate_command(command_id):
-            print command_id
-            self._execute(str(command_id))
+    def send_command(self, remote_id, key):
+        remote = self.config_parser.get_remote(remote_id)
+        key = str(key)
+
+        if remote and key in remote["keys"]:
+            cmd = "irsend SEND_ONCE " + remote["config_file"] + " " + key
+            subprocess.call(cmd, shell=True)
+            return True
         else:
-            print "Unknown commands, remote received following signal", arg
+            return False
 
-    def _execute(self, command_id):
-        cmd = "irsend SEND_ONCE " + self.conf_file + " " + str(command_id)
-        subprocess.call(cmd, shell=True)
+    def set_volume(self, remote_id, percentage):
+        remote = self.config_parser.get_remote(remote_id)
+
+        if remote:
+            try:
+                value = float(percentage) * 100.0
+                steps = value * self._max_steps
+                cmd = "irsend SEND_ONCE " + remote["config_file"] + " "
+
+                if value > 0:
+                    cmd += "KEY_VOLUMEUP"
+
+                    for i in range(0, steps):
+                        subprocess.call(cmd, shell=True)
+                else:
+                    cmd += "KEY_VOLUMEDOWN"
+
+                    for i in range(steps, 0, -1):
+                        subprocess.call(cmd, shell=True)
+
+            except ValueError:
+                abort(400)
